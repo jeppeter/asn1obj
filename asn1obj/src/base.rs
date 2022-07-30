bfab3052dfc5f1ec0c0f689b83ebe109b59a3aac
jeppeter <jeppeter@gmail.com>
2022-07-30 16:41:49 +0800

    make pass format for Asn1Time

--- asn1obj/src/base.rs
+++ asn1obj/src/base.rs
@@ -1,7 +1,7 @@
 
 
 use std::error::Error;
-use chrono::{Utc,DateTime,Datelike,Timelike};
+use chrono::{Utc,DateTime,Datelike,Timelike,Duration};
 use chrono::prelude::*;
 use crate::asn1impl::{Asn1Op};
 use crate::consts::{ASN1_PRIMITIVE_TAG,ASN1_CONSTRUCTED,ASN1_INTEGER_FLAG,ASN1_BOOLEAN_FLAG,ASN1_MAX_INT,ASN1_MAX_LONG,ASN1_MAX_INT_1,ASN1_MAX_INT_2,ASN1_MAX_INT_3,ASN1_MAX_INT_4,ASN1_MAX_INT_NEG_1,ASN1_MAX_INT_NEG_2,ASN1_MAX_INT_NEG_3,ASN1_MAX_INT_NEG_4,ASN1_MAX_INT_NEG_5,ASN1_MAX_INT_5,ASN1_BIT_STRING_FLAG,ASN1_OCT_STRING_FLAG,ASN1_NULL_FLAG,ASN1_OBJECT_FLAG,ASN1_ENUMERATED_FLAG,ASN1_UTF8STRING_FLAG,ASN1_IMP_FLAG_MASK,ASN1_PRINTABLE_FLAG,ASN1_TIME_FLAG,ASN1_TIME_DEFAULT_STR,ASN1_OBJECT_DEFAULT_STR};
@@ -1268,8 +1268,197 @@ pub struct Asn1Time {
 
 
 impl Asn1Time {
-	fn extract_date_value(&self,s :&str) -> Result<(i64,i64,i64,i64,i64),Box<dyn Error>> {
-		let c :String = "([0-9]{4})-([0-9]{2})-([0-9]{2}) ([0-9]{2}):([0-9]{2})".to_string();
+
+	fn parse_value(&self, s :&str) -> Result<i64,Box<dyn Error>> {
+		match i64::from_str_radix(s,10) {
+			Ok(v) => {				
+				return Ok(v);
+			},
+			Err(e) => {
+				asn1obj_new_error!{Asn1ObjBaseError,"parse [{}] error[{:?}]",s,e}
+			}
+		}
+	}
+
+	fn format_time_str(&self, year :i64, mon :i64,mday :i64,hour :i64, min :i64,sec :i64) -> String {
+		return format!("{:04}-{:02}-{:02} {:02}:{:02}:{:02}", year,mon,mday,hour,min,sec);
+	}
+
+	fn get_time_val(&self, times :&str) -> Result<(i64,i64,i64,i64,i64,i64),Box<dyn Error>> {
+		let mut year :i64;
+		let mut mon :i64;
+		let mut mday :i64;
+		let mut hour :i64;
+		let mut min :i64;
+		let mut sec :i64;
+		asn1obj_log_trace!("times [{}]", times);
+
+		if times.len() == 10 {
+			year = self.parse_value(&times[0..4])?;
+			mon = self.parse_value(&times[4..6])?;
+			mday = self.parse_value(&times[6..8])?;
+			hour = self.parse_value(&times[8..10])?;
+			min = 0;
+			sec = 0;
+			let ov = self.check_data_valid(year,mon,mday,hour,min,sec);
+			if ov.is_ok() {
+				return Ok((year,mon,mday,hour,min,sec));
+			}
+
+			/**/
+			year = self.parse_value(&times[0..2])?;
+			if year < 70 {
+				year += 2000; 
+			} else {
+				year += 1900;
+			}
+			mon = self.parse_value(&times[2..4])?;
+			mday = self.parse_value(&times[4..6])?;
+			hour = self.parse_value(&times[6..8])?;
+			min = self.parse_value(&times[8..10])?;
+			sec = 0;
+			let ov = self.check_data_valid(year,mon,mday,hour,min,sec);
+			if ov.is_err() {
+				let e = ov.err().unwrap();
+				return Err(e);
+			}
+			return Ok((year,mon,mday,hour,min,sec));
+		} 
+
+		if times.len() == 12 {
+			year = self.parse_value(&times[0..4])?;
+			mon = self.parse_value(&times[4..6])?;
+			mday = self.parse_value(&times[6..8])?;
+			hour = self.parse_value(&times[8..10])?;
+			min = self.parse_value(&times[10..12])?;
+			sec = 0;
+			let ov = self.check_data_valid(year,mon,mday,hour,min,sec);
+			if ov.is_ok() {
+				return Ok((year,mon,mday,hour,min,sec));
+			}
+
+			/**/
+			year = self.parse_value(&times[0..2])?;
+			if year < 70 {
+				year += 2000; 
+			} else {
+				year += 1900;
+			}
+			mon = self.parse_value(&times[2..4])?;
+			mday = self.parse_value(&times[4..6])?;
+			hour = self.parse_value(&times[6..8])?;
+			min = self.parse_value(&times[8..10])?;
+			sec = self.parse_value(&times[10..12])?;
+			let ov = self.check_data_valid(year,mon,mday,hour,min,sec);
+			if ov.is_err() {
+				let e = ov.err().unwrap();
+				return Err(e);
+			}
+			return Ok((year,mon,mday,hour,min,sec));
+		}
+
+		if times.len() >= 14 {
+			year = self.parse_value(&times[0..4])?;
+			mon = self.parse_value(&times[4..6])?;
+			mday = self.parse_value(&times[6..8])?;
+			hour = self.parse_value(&times[8..10])?;
+			min = self.parse_value(&times[10..12])?;
+			sec = self.parse_value(&times[12..14])?;
+			let ov = self.check_data_valid(year,mon,mday,hour,min,sec);
+			if ov.is_err() {
+				let e = ov.err().unwrap();
+				return Err(e);
+			}
+			return Ok((year,mon,mday,hour,min,sec));
+		}
+
+		asn1obj_new_error!{Asn1ObjBaseError,"not valid [{}] times", times}
+
+	}
+
+	fn extract_encode_value(&self, s :&str) -> Result<(i64,i64,i64,i64,i64,i64),Box<dyn Error>> {
+		let mut year :i64;
+		let mut mon :i64;
+		let mut mday :i64;
+		let mut hour :i64;
+		let mut min :i64;
+		let mut sec :i64;
+		let mut dt :DateTime<Utc>;
+
+		let c :String = "^(([0-9]+)(\\.[0-9]+)?(([-\\+])([0-9]+))?([Z|X]?))$".to_string();
+		let ro = Regex::new(&c);
+		if ro.is_err() {
+			let e = ro.err().unwrap();
+			asn1obj_new_error!{Asn1ObjBaseError,"regex [{}] error[{:?}]", c,e}
+		}
+		let reex = ro.unwrap();
+		let co = reex.captures(s);
+		if co.is_none() {
+			asn1obj_new_error!{Asn1ObjBaseError,"regex [{}] capture [{}] none", c,s}
+		}
+
+		asn1obj_log_trace!("encoded value [{}]",s);
+
+		let v = co.unwrap();
+		let times :String;
+		let zs :String;
+		let diffs :String;
+
+		times = format!("{}",v.get(2).map_or("", |m| m.as_str()));
+		if times.len() < 10 {
+			asn1obj_new_error!{Asn1ObjBaseError,"[{}] first part less < 10", s}
+		}
+
+		zs = format!("{}",v.get(7).map_or("", |m| m.as_str()));
+		diffs = format!("{}",v.get(4).map_or("", |m| m.as_str()));
+
+		(year,mon,mday,hour,min,sec) = self.get_time_val(&times)?;
+
+		if diffs.len() > 0  {
+			let plusorminus :String = format!("{}",v.get(5).map_or("", |m| m.as_str()));
+			let offstr :String = format!("{}",v.get(6).map_or("", |m| m.as_str()));
+			if zs.len() > 0 {
+				if zs == "Z" {
+					asn1obj_new_error!{Asn1ObjBaseError,"not valid time string [{}]",s}	
+				}				
+			}
+
+			if offstr.len() != 4 {
+				asn1obj_new_error!{Asn1ObjBaseError, "offstr [{}] != 4", offstr}
+			}
+
+			let voff = self.parse_value(&offstr[0..2])?;
+			if voff > 12 {
+				asn1obj_new_error!{Asn1ObjBaseError,"not valid offset [{}]",offstr}
+			}
+
+			dt = Utc.ymd(year as i32,mon as u32,mday as u32).and_hms(hour as u32,min as u32,sec as u32);
+			if plusorminus == "+" {
+				dt = dt - Duration::hours(voff);
+			} else {
+				dt = dt + Duration::hours(voff);
+			}
+			year = dt.year() as i64;
+			mon = dt.month() as i64;
+			mday = dt.day() as i64;
+			hour = dt.hour() as i64;
+			min = dt.minute() as i64;
+			sec= dt.second() as i64;
+		}
+
+		if zs.len() != 0 {
+			if zs != "Z" && zs != "X" {
+				asn1obj_new_error!{Asn1ObjBaseError,"not valid time [{}]",s}
+			}
+		}
+
+		Ok((year,mon,mday,hour,min,sec))
+
+
+	}
+
+	fn extract_date_value(&self,s :&str) -> Result<(i64,i64,i64,i64,i64,i64),Box<dyn Error>> {
+		let c :String = "([0-9]{4})-([0-9]{2})-([0-9]{2}) ([0-9]{2}):([0-9]{2})(:([0-9]{2}))?".to_string();
 		let ro = Regex::new(&c);
 		if ro.is_err() {
 			let e = ro.err().unwrap();
@@ -1281,8 +1470,8 @@ impl Asn1Time {
 			asn1obj_new_error!{Asn1ObjBaseError,"regex [{}] capture [{}] default [{}] none", c,s, ASN1_TIME_DEFAULT_STR}
 		}
 		let v = co.unwrap();
-		if v.len() < 6 {
-			asn1obj_new_error!{Asn1ObjBaseError,"regex [{}] capture [{}] default [{}] {:?} < 7", c,s, ASN1_TIME_DEFAULT_STR,v}
+		if v.len() < 8 {
+			asn1obj_new_error!{Asn1ObjBaseError,"regex [{}] capture [{}] default [{}] {:?} < 8", c,s, ASN1_TIME_DEFAULT_STR,v}
 		}
 
 		let year :i64;
@@ -1290,6 +1479,7 @@ impl Asn1Time {
 		let mday :i64;
 		let hour :i64;
 		let min :i64;
+		let sec :i64;
 		let mut cc :String;
 
 		cc = format!("{}",v.get(1).map_or("", |m| m.as_str()));
@@ -1341,11 +1531,26 @@ impl Asn1Time {
 				asn1obj_new_error!{Asn1ObjBaseError,"can not parse [{}] in [{}] error[{:?}]", s, cc,e}
 			}
 		}
-		Ok((year,mon,mday,hour,min))
+
+		cc = format!("{}", v.get(7).map_or("", |m| m.as_str()));
+		if cc.len() > 0 {
+			match i64::from_str_radix(&cc,10) {
+				Ok(v) => {
+					sec = v;
+				},
+				Err(e) => {
+					asn1obj_new_error!{Asn1ObjBaseError,"can not parse [{}] in [{}] error[{:?}]", s, cc,e}
+				}
+			}
+		} else {
+			sec = 0;
+		}
+
+		Ok((year,mon,mday,hour,min,sec))
 	}
 
-	fn check_data_valid(&self, year :i64, mon :i64,mday :i64,hour :i64, min :i64) -> Result<(),Box<dyn Error>> {
-		if year < 1900 {
+	fn check_data_valid(&self, year :i64, mon :i64,mday :i64,hour :i64, min :i64,sec :i64) -> Result<(),Box<dyn Error>> {
+		if year < 1900  ||  year > 2100 {
 			asn1obj_new_error!{Asn1ObjBaseError,"year [{}] < 1900" ,year}
 		}
 		if mon < 1 || mon > 12 {
@@ -1362,6 +1567,10 @@ impl Asn1Time {
 
 		if min < 0 || min > 59 {
 			asn1obj_new_error!{Asn1ObjBaseError,"min {} not valid", min}
+		}
+
+		if sec < 0 || sec > 59 {
+			asn1obj_new_error!{Asn1ObjBaseError,"sec {} not valid", sec}	
 		}
 
 		if (mon == 4 || mon == 6 || mon == 9 || mon == 11) && mday > 30 {
@@ -1385,9 +1594,9 @@ impl Asn1Time {
 	}
 
 	pub fn set_value_str(&mut self, s :&str) -> Result<(),Box<dyn Error>> {
-		let (year,mon,mday,hour,min) = self.extract_date_value(s)?;
-		let _ = self.check_data_valid(year,mon,mday,hour,min)?;
-		self.val = format!("{:04}-{:02}-{:02} {:02}:{:02}", year,mon,mday,hour,min);
+		let (year,mon,mday,hour,min,sec) = self.extract_date_value(s)?;
+		let _ = self.check_data_valid(year,mon,mday,hour,min,sec)?;
+		self.val = self.format_time_str(year,mon,mday,hour,min,sec);
 		Ok(())
 	}
 
@@ -1396,15 +1605,15 @@ impl Asn1Time {
 	}
 
 	pub fn set_value_time(&mut self,dt :&DateTime<Utc>) -> Result<(),Box<dyn Error>> {
-		let (year,mon,mday,hour,min) = (dt.year(),dt.month(),dt.day(),dt.hour(),dt.minute());
-		let _ = self.check_data_valid(year as i64,mon as i64,mday as i64,hour as i64,min as i64)?;
-		self.val = format!("{:04}-{:02}-{:02} {:02}:{:02}", year,mon,mday,hour,min);
+		let (year,mon,mday,hour,min,sec) = (dt.year(),dt.month(),dt.day(),dt.hour(),dt.minute(), dt.second());
+		let _ = self.check_data_valid(year as i64,mon as i64,mday as i64,hour as i64,min as i64,sec as i64)?;
+		self.val = self.format_time_str(year as i64,mon as i64,mday as i64,hour as i64,min as i64,sec as i64);
 		Ok(())
 	}
 
 	pub fn get_value_time(&self) -> Result<DateTime<Utc>,Box<dyn Error>> {
-		let (year,mon,mday,hour,min) = self.extract_date_value(&self.val)?;
-		let dt :DateTime<Utc> = Utc.ymd(year as i32,mon as u32,mday as u32).and_hms_milli(hour as u32,min as u32,0,0);
+		let (year,mon,mday,hour,min,sec) = self.extract_date_value(&self.val)?;
+		let dt :DateTime<Utc> = Utc.ymd(year as i32,mon as u32,mday as u32).and_hms(hour as u32,min as u32,sec as u32);
 		Ok(dt)
 	}
 }
@@ -1420,7 +1629,7 @@ impl Asn1Op for Asn1Time {
 
 	fn decode_asn1(&mut self,code :&[u8]) -> Result<usize,Box<dyn Error>> {
 		let retv :usize;
-		let (year,mon,mday,hour,min):(i64,i64,i64,i64,i64);
+		let (year,mon,mday,hour,min,sec):(i64,i64,i64,i64,i64,i64);
 		if code.len() < 2 {
 			asn1obj_new_error!{Asn1ObjBaseError,"len [{}] < 2", code.len()}
 		}
@@ -1441,69 +1650,13 @@ impl Asn1Op for Asn1Time {
 		let a = retm.freeze();
 
 		let s = String::from_utf8_lossy(&a).to_string();
-		let mut fmts :String = "".to_string();
 		if s.len() < 12 {
 			asn1obj_new_error!{Asn1ObjBaseError,"not valid string [{}]",s}
 		}
 
-		let c = s[0..4].to_string();
-		match i64::from_str_radix(&c,10) {
-			Ok(v) => {
-				year = v;
-				fmts.push_str(&format!("{:04}",v));
-			},
-			Err(e) => {
-				asn1obj_new_error!{Asn1ObjBaseError,"can not parse [{}] error[{:?}]", c,e}
-			}
-		}
-
-		let c = s[4..6].to_string();
-		match i64::from_str_radix(&c,10) {
-			Ok(v) => {
-				mon = v;
-				fmts.push_str(&format!("-{:02}",v));
-			},
-			Err(e) => {
-				asn1obj_new_error!{Asn1ObjBaseError,"can not parse [{}] error[{:?}]", c,e}
-			}
-		}
-
-		let c = s[6..8].to_string();
-		match i64::from_str_radix(&c,10) {
-			Ok(v) => {
-				mday = v;
-				fmts.push_str(&format!("-{:02}",v));
-			},
-			Err(e) => {
-				asn1obj_new_error!{Asn1ObjBaseError,"can not parse [{}] error[{:?}]", c,e}
-			}
-		}
-
-		let c = s[8..10].to_string();
-		match i64::from_str_radix(&c,10) {
-			Ok(v) => {
-				hour = v;
-				fmts.push_str(&format!(" {:02}",v));
-			},
-			Err(e) => {
-				asn1obj_new_error!{Asn1ObjBaseError,"can not parse [{}] error[{:?}]", c,e}
-			}
-		}
-
-
-		let c = s[10..12].to_string();
-		match i64::from_str_radix(&c,10) {
-			Ok(v) => {
-				min = v;
-				fmts.push_str(&format!(":{:02}",v));
-			},
-			Err(e) => {
-				asn1obj_new_error!{Asn1ObjBaseError,"can not parse [{}] error[{:?}]", c,e}
-			}
-		}
-
-		let _ = self.check_data_valid(year,mon,mday,hour,min)?;
-		self.val = fmts;
+		(year,mon,mday,hour,min,sec) = self.extract_encode_value(&s)?;
+		let _ = self.check_data_valid(year,mon,mday,hour,min,sec)?;
+		self.val = self.format_time_str(year,mon,mday,hour,min,sec);
 		asn1obj_log_trace!("Asn1Time {}",self.val);
 		self.data = Vec::new();
 		retv = hdrlen + totallen;
@@ -1519,8 +1672,8 @@ impl Asn1Op for Asn1Time {
 		let vcode :Vec<u8>;
 
 
-		let (year,mon,mday,hour,min) = self.extract_date_value(&self.val)?;
-		let s = format!("{:04}{:02}{:02}{:02}{:02}Z",year,mon,mday,hour,min);
+		let (year,mon,mday,hour,min,sec) = self.extract_date_value(&self.val)?;
+		let s = format!("{:04}{:02}{:02}{:02}{:02}{:02}Z",year,mon,mday,hour,min,sec);
 		vcode = s.as_bytes().to_vec();
 		llen = vcode.len() as u64;
 
